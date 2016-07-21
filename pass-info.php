be26ea85d6ff3f3693b34abb5039a87bd7def4bc
kuruma <kuruma@navit-j.com>
2016-07-21 13:02:53 +0900

    add new file

---
+++ pass-info.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>社内サイト一覧</title>
+    </head>
+    <body>
+        <table border=1>
+            <tr>
+                <th></th><th>アカウント</th><th>パスワード</th>
+            </tr>
+                <tr><td>行-1</td><td>A-1</td><td>B-1</td>   
+            </tr>
+                <tr><td>行-2</td><td>A-2</td><td>B-2</td>
+            </tr>
+                <tr><td>行-3</td><td>A-3</td><td>B-3</td>
+            </tr>
+        </table>        
+        <?php
+            echo "テスト";
+        // put your code here
+        ?>
+    </body>
+</html>
